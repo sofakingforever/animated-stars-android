afe75e44ccf6b5fb93279d7de76548d4b1a3852b
sofakingforever <sofakingforever@gmail.com>
2018-08-21 22:48:10 +0300

    Added Example Code

--- app/src/main/java/com/sofakingforever/animatedstarsview/MainActivity.kt
+++ app/src/main/java/com/sofakingforever/animatedstarsview/MainActivity.kt
@@ -2,11 +2,29 @@ package com.sofakingforever.animatedstarsview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        stars_white.onStart()
+        stars.onStart()
+    }
+
+    override fun onStop() {
+
+        stars_white.onStop()
+        stars.onStop()
+
+        super.onStop()
     }
 }
